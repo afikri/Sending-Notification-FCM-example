e9eccd06fe0aa836ba5704b4d836444c58760a6b
Fikri Abdul <fikri.abdul@gmx.com>
2016-10-31 08:59:01 +0700

    First commit

---
+++ app/src/main/java/com/afikri/firendroid/MyFirebaseInstanceIdService.java
@@ -0,0 +1,22 @@
+package com.afikri.firendroid;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+/**
+ * Created by afikri on 31/10/16.
+ */
+
+public class MyFirebaseInstanceIdService extends FirebaseInstanceIdService {
+    private static final String REG_TOKEN ="REG_TOKEN";
+    @Override
+    public void onTokenRefresh(){
+        String recentToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d(REG_TOKEN,recentToken);
+
+    }
+
+
+}
---
+++ app/src/main/java/com/afikri/firendroid/MyFirebaseMessagingService.java
@@ -0,0 +1,33 @@
+package com.afikri.firendroid;
+
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.NotificationCompat;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+/**
+ * Created by afikri on 31/10/16.
+ */
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService{
+    @Override
+    public void onMessageReceived(RemoteMessage rm){
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this,0,intent,PendingIntent.FLAG_ONE_SHOT);
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this);
+        notificationBuilder.setContentTitle("FCM Notification");
+        notificationBuilder.setContentText(rm.getNotification().getBody());
+        notificationBuilder.setAutoCancel(true);
+        notificationBuilder.setSmallIcon(R.mipmap.ic_launcher);
+        notificationBuilder.setContentIntent(pendingIntent);
+
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(0,notificationBuilder.build());
+
+    }
+}
